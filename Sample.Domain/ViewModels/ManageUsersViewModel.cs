613bab3f51d8af35e0b49e37473799e5014f622c
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2020-12-28 14:53:04 +0200

    Add view models

---
+++ Sample.Domain/ViewModels/ManageUsersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Sample.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Domain.ViewModels
+{
+    public class ManageUsersViewModel
+    {
+        public ApplicationUser[] Administrators { get; set; }
+        public ApplicationUser[] Everyone { get; set; }
+    }
+}
---
+++ Sample.Domain/ViewModels/TodoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Sample.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Domain.ViewModels
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] Items { get; set; }
+    }
+}
